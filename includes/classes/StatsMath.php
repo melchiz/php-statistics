9298c19beae02a91154b8eca86cae49411cfde1e
Melchizo <Melchizo@Melchiz>
2017-04-23 10:36:27 +0300

    PHP statistics programming project

--- includes/classes/StatsMath.php
+++ includes/classes/StatsMath.php
@@ -10,5 +10,38 @@
  * @author Melchizo
  */
 class StatsMath extends Math{
-    
+    /**
+     * 
+     * @param array $x an array of float data type
+     * @return float
+     */
+    public static function sum(array $x){
+        $s = 0;
+        foreach ($x as $value) {
+            $s += $value;
+        }
+        return $s;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function diff(array $x, array $y) {
+        $s = [];
+        for ($i = 0; $i < count($x); $i++) {
+            $x[i] = Math::sub($x, $y);
+        }
+        return $s;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function rem(float $x, float $y){
+        return $x % $y;
+    }
 }
