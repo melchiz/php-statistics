ff93fe58aadeb0bf4442795ff474715b83a60950
Melchizo <Melchizo@Melchiz>
2017-04-23 09:42:03 +0300

    PHP statistics programming project

---
+++ includes/classes/Math.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Math
+ *
+ * @author Melchiz Oyamo
+ */
+abstract class Math {
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function add(float $x, float $y){
+        return $x + $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function sub(float $x, float $y){
+        return $x - $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function mul(float $x, float $y){
+        return $x * $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function div(float $x, float $y){
+        return $x / $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function mod(float $x, float $y){
+        return $x % $y;
+    }
+   /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function sum(float $x, float $y){
+        return $x + $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function diff(float $x, float $y){
+        return $x - $y;
+    }
+    /**
+     * 
+     * @param float $x a float data type
+     * @param float $y a float data type
+     * @return float
+     */
+    public static function rem(float $x, float $y){
+        return $x % $y;
+    }
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mel Stats</title>
+    </head>
+    <body>
+        <?php
+        include_once './includes/classes/Math.php';
+        echo "This is it <br>";
+        $x = 4646;
+        $y = 465;
+        echo 'Add(23, 34): ' . Math::add(23.05, 34.05);
+        echo 'Add(23, 34): ' . Math::div($x, $y);
+        ?>
+    </body>
+</html>
